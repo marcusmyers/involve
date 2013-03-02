ef2b484196687ef9a2f562a25b158c2804fe2109
Mark Myers <marcusmyers@gmail.com>
2013-03-02 14:28:24 -0500

    Added a basic layout for the sign-in and signup pages

---
+++ application/views/layouts/basic.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Involve</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="iPads Signout Web App to see how many are available for the building">
+    <meta name="author" content="Technology Department">
+
+    <!-- Le styles -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/default.css">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
