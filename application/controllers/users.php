9c6cd2c0335932d86b012137f1f721c846b10bea
Mark Myers <marcusmyers@gmail.com>
2013-03-02 14:55:45 -0500

    added parent action to Users controller

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,4 +1,12 @@
 <?
 class Users_Controller extends Base_Controller {
+
+	public function action_parent(){
+		return View::make('users.parent');
+	}
+
+	public function action_signup(){
+
+	} 
 	
 }
