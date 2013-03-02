d63f0a8c0982c8ffee06d44069bad75910785d47
Chris Wenner <crwenner@gmail.com>
2013-03-02 16:13:38 -0500

    users

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -2,21 +2,16 @@
 
 class Users_Controller extends Base_Controller {
 
-<<<<<<< HEAD
-	public function action_parent()
-	{
-=======
 	public function action_index(){
 		echo "Test";
 	}
 
 	public function action_parent(){
->>>>>>> f8c02eda5a39dcb859a57ff3a6ef6e7b32f86442
 		return View::make('users.parent');
 	}
 
 	public function action_signup(){
 
 	} 
-	
+
 }
