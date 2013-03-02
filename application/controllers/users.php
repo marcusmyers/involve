d40a38c0132515279bcc928619038eecbb6119f9
Mark Myers <marcusmyers@gmail.com>
2013-03-02 15:37:40 -0500

    Fixed Chris' issues

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,5 +1,10 @@
 <?php
+
 class Users_Controller extends Base_Controller {
+
+	public function action_index(){
+		echo "Test";
+	}
 
 	public function action_parent(){
 		return View::make('users.parent');
