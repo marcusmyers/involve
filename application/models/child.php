2dcf65e397a1cc63468e3680b5015b48abd212f2
Chris Wenner <crwenner@gmail.com>
2013-03-03 00:58:22 -0500

    added color class for active

--- application/models/child.php
+++ application/models/child.php
@@ -2,10 +2,5 @@
 
 class Child extends Eloquent {
 	public static $timestamps = false;
-	public static $table = "Child";
 
-	$result = brando50_involve::Child('table')
-  ->where('thing', '=', $thing)
-  ->where('thing2', '=', $thing2)
-  ->first();
 }
