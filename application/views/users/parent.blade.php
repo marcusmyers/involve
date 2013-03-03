d7a12c0f7927a609f20576eb0ec519ebf397baa5
Chris Wenner <crwenner@gmail.com>
2013-03-02 22:39:28 -0500

    added styles

--- application/views/users/parent.blade.php
+++ application/views/users/parent.blade.php
@@ -1,4 +1,32 @@
 @layout('layouts/parent')
 @section('content')
-	<h1>Test 1 2 </h1>
+
+	<h2>Hello!</h2>
+
+	<p>
+		And welcome to your "insert clever app name here" app. This is a place where you can manage 
+		everything on your account. Just select from the following options to get started!
+	</p>
+	<hr>
+	<div id='main-container'>
+		<ul class='main-choice'>
+          <li id='block-me'>
+          	<a href="#">
+          		<h2>Kids</h2>
+          	</a>
+          </li>
+          
+          <li id='block-me'>
+          	<a href="#">
+          		<h2>Nanny</h2>
+          	</a>
+          </li>
+          
+          <li id='block-me'>
+          	<a href="#">
+          		<h2>Profile</h2>
+          	</a>
+      	  </li>
+        </ul>
+	</div>
 @endsection
