a9b51c669956ce1f5cc85ae5fa0b366eeda75553
Chris Wenner <crwenner@gmail.com>
2013-03-03 01:43:06 -0500

    created model for child view, need to create controller yet

--- application/models/child.php
+++ application/models/child.php
@@ -2,5 +2,20 @@
 
 class Child extends Eloquent {
 	public static $timestamps = false;
+	public static $table = "Child";
 
+	public function user()
+	{
+		return $this->has_many_and_belongs_to('User');
+	}
+
+	public function calendarEvent()
+	{
+		return $this->has_many_and_belongs_to('CalendarEvent');
+	}
+
+	public function medical()
+	{
+		return $this->has_many_and_belongs_to('Medical');
+	}
 }
